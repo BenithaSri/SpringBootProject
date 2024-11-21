e2a0d5b7df1561d6a907269c50b7fed8cc4b313a
BenithaSri <156029071+BenithaSri@users.noreply.github.com>
2024-11-21 11:12:09 -0600

    first commit

---
+++ src/main/java/com/benithaPanchagiri/DMS/Repository/DogRepository.java
@@ -0,0 +1,9 @@
+package com.benithaPanchagiri.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.benithaPanchagiri.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+
+}
